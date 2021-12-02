11db0d093085ba20780d7f5d815bf6c065c8b6a7
nomurabbit <nomurabbit@gmail.com>
2021-12-03 05:02:13 +0900

    20211203_01

--- 20211117_my_react_app_mui5/src/App.tsx
+++ 20211117_my_react_app_mui5/src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home';
+import Portrait from './pages/Portrait';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/web/portrait" element={<Portrait/>} />
+        <Route path="/web/home" element={<Home/>} />
+        <Route path="/web/" element={<Home/>} />
+        <Route path="*" element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ 20211117_my_react_app_mui5/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import ResponsiveDrawer from '../components/layout/ResponsiveDrawer';
+
+const Home = () => {
+  return (
+    <ResponsiveDrawer>
+      <div>
+        <p>It's home page.</p>
+      </div>
+    </ResponsiveDrawer>
+  );
+}
+  
+export default Home;
